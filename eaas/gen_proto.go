a75d9dd826f801f249b48333298ba2675d4838d4
Carl Kittelberger <icedream@icedream.pw>
2024-03-15 16:13:41 +0100

    Add reconstructed protobuf definitions and go code for it.

---
+++ eaas/gen_proto.go
@@ -0,0 +1,3 @@
+package server
+
+//go:generate go run -mod=mod github.com/bufbuild/buf/cmd/buf generate
---
+++ tools.go
@@ -0,0 +1,8 @@
+//go:build tools
+// +build tools
+
+package stagelinq
+
+import (
+	_ "github.com/bufbuild/buf/cmd/buf"
+)
