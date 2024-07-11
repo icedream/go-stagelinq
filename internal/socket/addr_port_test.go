ced48252f03676746a353b5e3b5838f609e6273b
Carl Kittelberger <icedream@icedream.pw>
2024-07-11 23:05:56 +0200

    Update GetPortFromAddress test to compare against uint16.

--- internal/socket/addr_port_test.go
+++ internal/socket/addr_port_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func Test_GetPortFromAddress(t *testing.T) {
-	require.Equal(t, 12345, GetPortFromAddress(&net.TCPAddr{
+	require.Equal(t, uint16(12345), GetPortFromAddress(&net.TCPAddr{
 		IP:   net.IPv4(1, 2, 3, 4),
 		Port: 12345,
 	}))
-	require.Equal(t, 12345, GetPortFromAddress(&net.UDPAddr{
+	require.Equal(t, uint16(12345), GetPortFromAddress(&net.UDPAddr{
 		IP:   net.IPv4(1, 2, 3, 4),
 		Port: 12345,
 	}))
