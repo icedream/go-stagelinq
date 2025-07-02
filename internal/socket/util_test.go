ef862859fcfc4ce2128f17687a11fd826ee2fb3d
Carl Kittelberger <icedream@icedream.pw>
2025-07-03 00:30:48 +0200

    Fix test name for MakeBroadcastIP.

--- internal/socket/util_test.go
+++ internal/socket/util_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_makeBroadcastIP(t *testing.T) {
+func Test_MakeBroadcastIP(t *testing.T) {
 	testValues := []struct {
 		IP            net.IP
 		Mask          net.IPMask
